56d7763dc36dbd2bcdd1c2b0c53f27dde7f94571
Ergaf <bymbarawko@gmail.com>
2020-03-24 01:39:40 +0300

    старый сайтик с модалочкой(не работает на гитхабе)

---
+++ js/modal.js
@@ -0,0 +1,45 @@
+let elements1 = document.querySelector('.modal-overlay');
+let elements2 = document.querySelector('.modal');
+
+document.querySelector('#zamovlenia').addEventListener("click", function(){
+    console.log("lol");
+    elements1.classList.add('active');
+    elements2.classList.add('active');
+});
+
+document.querySelector('.close-modal').addEventListener("click",function(){
+    elements1.classList.remove('active');
+    elements2.classList.remove('active');
+});
+
+document.querySelector(".promofor").addEventListener("click", function () {
+    $(".modal-content form").submit();
+});
+
+
+$(function() {
+
+    // E-mail Ajax Send
+    // https://denis-creative.com/contact-form-with-attached-file/
+    // http://shpargalkablog.ru/2014/05/email-file-php.html
+    // http://shpargalkablog.ru/2014/05/feedback-form-file.html
+    document.getElementById('feedback-form').addEventListener('submit', function(evt){
+        var http = new XMLHttpRequest(), f = this;
+        var th = $(this);
+        evt.preventDefault();
+        http.open("POST", "mail.php", true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+                if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
+                    th.trigger("reset");
+                }
+            }
+        }
+        http.onerror = function() {
+            alert('Извините, данные не были переданы');
+        }
+        http.send(new FormData(f));
+    }, false);
+
+});
